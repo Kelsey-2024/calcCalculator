45e1300faac6e49a8a31fd25693c16210f1361b4
Kelsey Cordero <kcordero99@gmail.com>
2024-03-06 17:22:56 -0500

    Pseudocode with some coding

---
+++ index.js
@@ -0,0 +1,51 @@
+/*Create a calculator using the following functions
+- getSum - adds two numbers
+- getDividend - divides two numbers
+- getProduct - multiplies two numbers
+- getDifference - subtracts two numbers
+- inverseNumber - returns the number as a positive or negative based on input
+- convertPercentageToDecimal - takes in a percentage and returns the number as a decimal
+- getRandomInteger - gets a random integer
+- getPi - returns 3.14
+- getRemainder - returns the remainder if two numbers are divided
+- getSquareRoot - returns the square root of a number
+- Do not show -0
+- Return a helpful message instead of NaN if there is an error
+When done writing the functions, invoke the functions multiple times and use the results to call other functions
+*/
+
+//create a function called calcCalculate
+//the parameter should take in two numbers --not sure which to use (a, b) or (inputOne, inputTwo) - tested
+const calcCalculate = (a, b) => {
+  
+}
+
+//Create separate functions outside the calcCalculate function
+  //create a function called getSum that adds two numbers
+const getSum = (a, b) => {
+  console.log(a+b);
+}
+
+getSum(4,6);
+
+  //create a function called getDividend that divides two numbers
+  //create a function called getProduct that multiples two numbers
+  //create a function called getDifference that subtracts two numbers
+  //create a function called inverseNumber that returns a positive or negative # based on input
+  //create a function called convertPercentageToDecimal that takes in a percentage and returns the number as a decimal
+  //create a function called getRandomInteger that gets a random integer
+  //create a function called getPi that returns 3.14
+  //create a function called getRemainder returns the remainder when two numbers are divided
+  //create a function called getSquareRoot returns square root of a number
+
+calcCalculate(); //calling || invoking the function
+
+
+//like an actual calculator we'll have "buttons"
+  //each button will be an if statement for the following functions
+  // **pause**
+
+
+
+//Write a helpful message as my else statement > would we do a nested if > else statement?
+
